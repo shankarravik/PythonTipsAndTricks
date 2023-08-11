b69d45c083ff6f8ac132d8248027a3bfb1f94006
shankarravik <shankarravik@yahoo.co.in>
2023-08-10 20:07:51 -0500

    How To Use ZIP Function In Python

---
+++ ZipFunction.py
@@ -0,0 +1,24 @@
+# How to use the Zip funtion in python
+
+Name = [
+    "Mark",
+    "Mike",
+    "John"
+    ]
+
+DOB = [
+        "10-05-1986",
+        "15-06-1987",
+        "18-07-1988"
+    ]
+
+Course = [
+        "Python",
+        "Java",
+        "SQL"
+    ]
+
+Student_Details = zip(Name, DOB, Course)
+print(list(Student_Details))
+
+
