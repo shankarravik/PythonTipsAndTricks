fd6d4f91fc2870531996ed61f57f16ea319d14f0
shankarravik <shankarravik@yahoo.co.in>
2023-08-10 19:26:24 -0500

    Zip Funcion

---
+++ ZipFunction.py
@@ -0,0 +1,6 @@
+#Student File
+Name = ["Mark", "Mike", "John"]
+DOB = ["10-05-1986","15-06-1987","18-07-1988"]
+Course = ["Python", "Java", "SQL"]
+Student_Details = zip(Name, DOB, Course)
+print (list(Student_Details))
